0d22c705e7c5d0efaed0fff6973901c7b396c093
gilberto <gilberto.santamaria@apriorins.com>
2023-05-14 22:23:39 -0500

    [ADD]more fields and login and logout option

--- js/index.js
+++ js/index.js
@@ -1,27 +1,85 @@
+// Vamos a crear un objeto usuario para poder administrar mejor la informacion
+let userModel = {
+    userName: '',
+    userPassword: '',
+    userEmail: '',
+    isUserLogged: false,
+    userImage: '',
+    userPhone: '',
+    userAge: '',
+    competitionData: {
+        position: '',
+        contestId: '',
+        time: '',
+        velocity: ''
+    }
+}
 //Cuando se acabe de cargar la pagina,  verificamos si el usuario esta loggeado
+let userStorage = window.localStorage;//aca vamos a guardar la sesion del usuario
 window.addEventListener('load', function () {
-    if (userStorage.getItem('_user_email') != null) {
+    userStorage.getItem('users_data') == null && userStorage.setItem('users_data', JSON.stringify({ users: [] }));
+    if (JSON.parse(userStorage.getItem('users_data')).users.length > 0) {
         //vamos a la pagina de competicion
-        location.href = '/user.html';
+        JSON.parse(userStorage.getItem('users_data')).users.forEach((user, index) => {
+            //debugger;
+            if (user.isUserLogged) {
+                location.href = '/user.html';
+            }
+        });
     }
 });
 
 //Vamos a guradar el formulario en una varialble
-let form = document.getElementById('register_form');
-let userStorage = window.localStorage;//aca vamos a guardar la sesion del usuario
+let registerForm = document.getElementById('register_form');
+let loginForm = document.getElementById('login_form');
 
 //Vamos a escuchar cuando el evento submit sea disparado 
-form.addEventListener('submit', function (event) {
+registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
     //vamos a crear un locastorage para guardar la sesion del usuario con el correo
     //Guardamos el valor del input correo y loso otros datos
-    let userEmailValue = document.getElementById('userEmail').value;
-    let userNameValue = document.getElementById('userName').value;
-    let userPhoneValue = document.getElementById('userPhone').value;
+    let userEmailValue = document.getElementById('registerUserEmail').value;
+    let userNameValue = document.getElementById('registerUserName').value;
+    let userPhoneValue = document.getElementById('registerUserPhone').value;
+    let userPasswordValue = document.getElementById('registerUserPassword').value;
+    let registeredUser = JSON.parse(JSON.stringify(userModel));
+    registeredUser['userName'] = userNameValue;
+    registeredUser['userEmail'] = userEmailValue;
+    registeredUser['userPhone'] = userPhoneValue;
+    registeredUser['userPassword'] = userPasswordValue;
+    registeredUser['isUserLogged'] = true;
     //Guardamos los valores en el localStorage
-    localStorage.setItem('_user_email', userEmailValue);
-    localStorage.setItem('_user_name', userNameValue);
-    localStorage.setItem('_user_phone', userPhoneValue);
+
+    let parsedData = JSON.parse(userStorage.getItem('users_data'));
+    parsedData.users.push(registeredUser);
+    userStorage.setItem('users_data', JSON.stringify(parsedData));
     //vamos a la pagina de competicion
     location.href = '/user.html';
+});
+//
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    //primero verificamos si el usuario existe
+    let loggedUserMail = document.getElementById('userEmail');
+    let loggedUserPass = document.getElementById('userPassword');
+    let userMailIndex;
+    let userExist = JSON.parse(userStorage.getItem('users_data')).users.some((user, index) => {
+        userMailIndex = index;
+        return user.userEmail == loggedUserMail.value
+    });
+    let userPassIndex;
+    let userCorrectPassWord = JSON.parse(userStorage.getItem('users_data')).users.some((user, index) => {
+        userPassIndex = index;
+        return user.userPassword == loggedUserPass.value
+    });
+    if (userExist) {
+        if (userCorrectPassWord) {
+            if (userMailIndex == userPassIndex) {
+                let parsedData = JSON.parse(userStorage.getItem('users_data'));
+                parsedData.users[userMailIndex].isUserLogged = true;
+                userStorage.setItem('users_data', JSON.stringify(parsedData));
+                location.href = '/user.html';
+            }
+        }
+    }
 });
--- js/user.js
+++ js/user.js
@@ -1,21 +1,37 @@
 
 //Cuando se acabe de cargar la pagina,  verificamos si el usuario esta loggeado
+let userLoggedIndex;
+let userStorage = window.localStorage;
 window.addEventListener('load', function () {
-    if (userStorage.getItem('_user_email') == null) {
+    if (!JSON.parse(userStorage.getItem('users_data')).users.length > 0) {
         //vamos a la pagina de inicio si el usuario no esta loggeado
         location.href = '/index.html';
     }
+    JSON.parse(userStorage.getItem('users_data')).users.forEach((user, index) => {
+        //debugger;
+        if (user.isUserLogged) {
+            userLoggedIndex = index;
+            //Si el usuario esta registrado, mostramos sus datos
+            let userData = document.getElementById('data_form');
+            let userStorage = window.localStorage;//aca vamos a guardar la sesion del usuario
+            let userName = document.getElementById('userName');
+            let userEmail = document.getElementById('userEmail');
+            let userPhone = document.getElementById('userPhone');
+
+            //debugger;
+            userName.value = JSON.parse(userStorage.getItem('users_data')).users[userLoggedIndex].userName;
+            userEmail.value = JSON.parse(userStorage.getItem('users_data')).users[userLoggedIndex].userEmail;
+        }
+    });
 });
 
-//Si el usuario esta registrado, mostramos sus datos
-let userData = document.getElementById('data_form');
-let userStorage = window.localStorage;//aca vamos a guardar la sesion del usuario
-let userName = document.getElementById('userName');
-let userEmail = document.getElementById('userEmail');
-let userPhone = document.getElementById('userPhone');
+function userLogOut() {
+    let parsedData = JSON.parse(userStorage.getItem('users_data'));
+    parsedData.users[userLoggedIndex].isUserLogged = false;
+    userStorage.setItem('users_data', JSON.stringify(parsedData));
+    location.href = '/index.html';
+}
 
-userName.value = userStorage.getItem('_user_name');
-userEmail.value = userStorage.getItem('_user_email');
-userPhone.value = userStorage.getItem('_user_phone');
+
 
 
